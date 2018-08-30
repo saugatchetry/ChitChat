69c5bfbd9b450ecd55e47e1318336d6e0232a25c
saugatchetry <saugatpkchetry@gmail.com>
2018-08-30 02:05:30 -0400

    added tabs and fragments

--- app/src/main/java/saugatchetry/com/chitchat/MainActivity.java
+++ app/src/main/java/saugatchetry/com/chitchat/MainActivity.java
@@ -1,13 +1,33 @@
 package saugatchetry.com.chitchat;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+
+    private TabsAccessorAdapter myTabsAccessorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("ChitChat");
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 }
---
+++ app/src/main/java/saugatchetry/com/chitchat/TabsAccessorAdapter.java
@@ -0,0 +1,51 @@
+package saugatchetry.com.chitchat;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdapter extends FragmentPagerAdapter {
+    public TabsAccessorAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+
+        switch (i){
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 1:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+            case 2:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+            default:
+                    return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Chats";
+            case 1:
+                return "Contacts";
+            case 2:
+                return "Groups";
+            default:
+                return null;
+        }
+    }
+}
