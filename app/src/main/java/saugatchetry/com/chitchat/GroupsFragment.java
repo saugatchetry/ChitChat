f23aefa00235ab41b8279e7bb3beba15cd342af3
saugatchetry <saugatpkchetry@gmail.com>
2018-09-02 18:36:03 -0400

    new group creating done

--- app/src/main/java/saugatchetry/com/chitchat/GroupsFragment.java
+++ app/src/main/java/saugatchetry/com/chitchat/GroupsFragment.java
@@ -6,6 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 
 /**
@@ -13,6 +26,13 @@ import android.view.ViewGroup;
  */
 public class GroupsFragment extends Fragment {
 
+    private View groupFragmentView;
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> groupList = new ArrayList<>();
+
+    private DatabaseReference rootReference;
 
     public GroupsFragment() {
         // Required empty public constructor
@@ -22,8 +42,48 @@ public class GroupsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+
+        groupFragmentView =  inflater.inflate(R.layout.fragment_groups, container, false);
+
+        rootReference = FirebaseDatabase.getInstance().getReference().child("Groups");
+
+        initializeUIComponents();
+
+        getGroupNamesFromBackend();
+
+        return groupFragmentView;
+    }
+
+    private void getGroupNamesFromBackend() {
+
+        rootReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Set<String> set = new HashSet<>();
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+
+                while(iterator.hasNext()){
+                    set.add(((DataSnapshot)iterator.next()).getKey());
+                }
+
+                groupList.clear();
+                groupList.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void initializeUIComponents() {
+
+        listView = (ListView) groupFragmentView.findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, groupList);
+        listView.setAdapter(arrayAdapter);
     }
 
 }
--- app/src/main/java/saugatchetry/com/chitchat/MainActivity.java
+++ app/src/main/java/saugatchetry/com/chitchat/MainActivity.java
@@ -1,16 +1,23 @@
 package saugatchetry.com.chitchat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -125,7 +132,53 @@ public class MainActivity extends AppCompatActivity {
         else if(item.getItemId() == R.id.main_find_friends_option){
 
         }
+        else if(item.getItemId() == R.id.main_create_group_option){
+            createNewGroup(); // requestNewGroup()
+        }
 
         return true;
+    }
+
+    private void createNewGroup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.AlertDialog);
+        builder.setTitle("Enter the Group Name");
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("eg. My Bewakoof Friends");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String groupName = groupNameField.getText().toString();
+                if(TextUtils.isEmpty(groupName)){
+                    Toast.makeText(MainActivity.this, "Please Enter a Group Name", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    addNewGroupToBackEnd(groupName);
+                }
+            }
+        });
+
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void addNewGroupToBackEnd(final String groupName) {
+
+        rootReference.child("Groups").child(groupName).setValue("")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, groupName+ " create successfully ", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
     }
 }
