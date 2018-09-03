48090670dec148b1ee3885c1b2996fecca98cdcd
saugatchetry <saugatpkchetry@gmail.com>
2018-09-03 19:30:06 -0400

    group messages retrieved from backend and displayed

--- app/src/main/java/saugatchetry/com/chitchat/GroupChatActivity.java
+++ app/src/main/java/saugatchetry/com/chitchat/GroupChatActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class GroupChatActivity extends AppCompatActivity {
 
@@ -57,12 +59,64 @@ public class GroupChatActivity extends AppCompatActivity {
             public void onClick(View view) {
                 saveMessageToDatabase();
                 sendMessageEditText.setText("");
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
             }
         });
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        groupNameReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.exists()){
+                    displayMessages(dataSnapshot);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.exists()){
+                    displayMessages(dataSnapshot);
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void displayMessages(DataSnapshot dataSnapshot) {
+
+        Iterator iterator = dataSnapshot.getChildren().iterator();
+
+        while(iterator.hasNext()){
+            String chatDate = (String) ((DataSnapshot) iterator.next()).getValue();
+            String chatMessage = (String) ((DataSnapshot) iterator.next()).getValue();
+            String chatName = (String) ((DataSnapshot) iterator.next()).getValue();
+            String chatTime = (String) ((DataSnapshot) iterator.next()).getValue();
+
+            displayTextMessages.append(chatName+ " :\n"+ chatMessage + "\n" + chatTime + "    "+chatDate+"\n\n\n");
+
+            scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+        }
+
+    }
 
     private void initializeUIComponents() {
         toolbar = (Toolbar) findViewById(R.id.group_chat_bar_layout);
